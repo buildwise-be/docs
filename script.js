6c36475eda6d7559948d41e113fb1178129d58bc
DavidMeurisse <david.meurisse@buildwise.be>
2026-04-09 18:44:46 +0200

    Add GTM integration to docs.json and script.js
    Mintlify-Source: dashboard-editor

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-console.log("page loaded");
-document.addEventListener('DOMContentLoaded', () => {
-    
-    // GTM page_loaded event
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-        event: 'page_loaded',
-        meta: {
-            event_id: crypto.randomUUID(),
-            user_id: ''    // internal user identifier
-        },
-        page: {
-            location: document.location.href.toLowerCase(),
-            title: document.title.toLowerCase(),
-            referrer: document.referrer.toLowerCase()
-        }
-    });
-    // End GTM page_loaded event
-});
+// GTM page_loaded event
+const gtml_page_loaded = {
+    event: 'page_loaded',
+    meta: {
+        event_id: crypto.randomUUID(),
+        user_id: ''    // internal user identifier
+    },
+    page: {
+        location: document.location.href.toLowerCase(),
+        title: document.title.toLowerCase(),
+        referrer: document.referrer.toLowerCase()
+    }
+}
+
+console.log("page loaded", gtml_page_loaded);
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push(gtml_page_loaded);
